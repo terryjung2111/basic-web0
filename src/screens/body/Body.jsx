aecf54a40bba4bb61bea96a840525e2e6958935c
Terry <terryjung2111@gmail.com>
2021-08-18 14:28:11 -0400

    structure0

---
+++ src/screens/body/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div>
+      This is Body section
+    </div>
+  )
+}
+
+export default Body;
---
+++ src/screens/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      This is Header section
+    </div>
+  )
+}
+
+export default Header;
