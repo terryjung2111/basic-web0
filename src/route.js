6ea574600a5bd3f6e51d0fca5fee0f0007a08d0a
Terry <terryjung2111@gmail.com>
2021-08-20 14:48:19 -0400

    structure1

---
+++ src/route.js
@@ -0,0 +1,7 @@
+export const routeList = [
+  {path: "/", title: "Home"},
+  {path: "/about", title: "About"},
+  {path: "/contact", title: "Contact"},
+  {path: "/project", title: "Project"},
+];
+
