4b0c421186c0ed33db7d096f25e4ae3f26f4fdf9
Terry <terryjung2111@gmail.com>
2021-08-21 22:31:35 -0400

    structure2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import Header from './screens/header/Header';
-import Body from './screens/body/Body';
 import Footer from './screens/footer/Footer';
+import About from './screens/body/About';
+import Contact from './screens/body/Contact';
+import Project from './screens/body/Project';
+import Home from './screens/body/Home';
+import {Route, Switch} from 'react-router-dom';
+
 
 const App = () => {
   return (
@@ -12,7 +17,20 @@ const App = () => {
       </div>
       {/*Body*/}
       <div style={{ border: '2px solid blue' }}>
-        <Body />
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/contact">
+            <Contact/>
+          </Route>
+          <Route path="/project">
+            <Project/>
+          </Route>
+        </Switch>
       </div>
       {/*Footer*/}
       <div style={{ border: '2px solid green' }}>
--- src/screens/header/Header.jsx
+++ src/screens/header/Header.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
-
+import logoImage from '../../resources/images/send.png'
+import mapImage from '../../resources/images/map.png'
+import "./Header.css"
+import { routeList } from '../../route'
+import { useHistory } from 'react-router-dom';
 const Header = () => {
+
+  const history = useHistory();
+
+  const onClickRoute = (path) => {
+    history.push(path)
+  }
+
   return (
     <div>
-      This is Header section
+      <div className="header-container">
+        <div>
+          <img src={logoImage} alt="This is logo image" style={{ width: "auto", height: "50px" }}/>
+        </div>
+        <div style={{ display: 'flex' }}>
+          {/* map은 for문이랑 같은 기능. 즉 차례대로 반복해서 나오는것*/}
+          {routeList.map((element, index) => {
+            return (         
+              <div key={index} style={{ margin: '0 10px'}} onClick={() => onClickRoute(element.path)}>
+                {element.title}
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
+    
   )
 }
 
